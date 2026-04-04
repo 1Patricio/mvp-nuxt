ab4e6a2ed5976a51b186471ac878377725280d8b
Anderson Patricio <andersongpatricio@hotmail.com>
2026-04-03 22:01:06 -0300

    feat: adicionando SEO e OGImage

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,12 @@
     <NuxtPage />
   </UApp>  
 </template>
+
+<script setup lang="ts">
+defineOgImageComponent('NuxtSeo', {
+  title: 'Anderson Patricio',
+  description: 'Software Engineer',
+  theme: '#111111', 
+  colorMode: 'dark'
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,33 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   ssr: true,
-  ui: {
-    colorMode: true
-  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/hints',
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/ui',
-  ]
+    '@nuxtjs/seo'
+  ],
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'keywords',
+          content: 'nuxt portfolio, nuxt develper, nuxt ui example'
+        },
+      ]
+    }
+  },
+  site:{
+    url: 'https://portifolio-anderson-patricio.netlify.app',
+    name: 'Portfolio Anderson Patricio',
+    description: 'Portfolio created with Vue, Nuxt, and Nuxt UI.',
+    keywords: 'vue, vuejs, nuxt, nuxtui'
+  },
+  ogImage: {
+    enabled: true
+  }
 })
